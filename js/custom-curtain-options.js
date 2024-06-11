955f2838642a233c71aa547d199fa6c36b5d0d3b
mufaqar <mufaqar@gmail.com>
2024-06-11 15:54:14 +0500

    working code pipe

--- js/custom-curtain-options.js
+++ js/custom-curtain-options.js
@@ -4,19 +4,25 @@ jQuery(document).ready(function($) {
             '5_3_58_4_57': 4.05,
             '6_3_69_4_68': 4.86,
             '9_3_105_4_104': 7.28,
-            '12_3_141_4_140': 9.71
+            '12_3_141_4_140': 9.71,
+            'ln_ft_price': 0.54,
+            'pipe_pocket_price': 1.92
         },
         '18_white': {
             '5_3_58_4_57': 5.04,
             '6_3_69_4_68': 6.05,
             '9_3_105_4_104': 9.07,
-            '12_3_141_4_140': 12.10
+            '12_3_141_4_140': 12.10,
+            'ln_ft_price': 0.61,
+            'pipe_pocket_price': 2.16
         }
     };
 
     function updatePrice() {
         const material = $('#curtain_material').val();
         const size = $('#curtain_size').val();
+        const secondHem = $('#second_hem').val();
+        const pipePocket = parseInt($('#pipe_pocket').val());
         let price = 0;
 
         if (size === 'custom') {
@@ -31,10 +37,22 @@ jQuery(document).ready(function($) {
             price = priceMap[material][size] || 0;
         }
 
+        // Calculate additional cost for the second hem
+        if (secondHem !== 'none') {
+            const length = size === 'custom' ? parseFloat($('#custom_length').val()) : parseFloat(size.split('_')[0]);
+            price += length * priceMap[material]['ln_ft_price'];
+        }
+
+        // Calculate additional cost for the pipe pocket
+        if (!isNaN(pipePocket) && pipePocket > 0) {
+            const length = size === 'custom' ? parseFloat($('#custom_length').val()) : parseFloat(size.split('_')[0]);
+            price += length * priceMap[material]['pipe_pocket_price'] * pipePocket;
+        }
+
         $('#curtain_price').text('$' + price.toFixed(2));
     }
 
-    $('#curtain_material, #curtain_size, #custom_length, #custom_width').on('change keyup', updatePrice);
+    $('#curtain_material, #curtain_size, #custom_length, #custom_width, #second_hem, #pipe_pocket').on('change keyup', updatePrice);
 
     $('#curtain_size').on('change', function() {
         if ($(this).val() === 'custom') {
