b40974fa273184e422c1bd5bc47b5a33183740d9
mufaqar <mufaqar@gmail.com>
2024-08-22 18:22:41 +0500

    enquey script

--- includes/enqueue-scripts.php
+++ includes/enqueue-scripts.php
@@ -2,20 +2,13 @@
 if (!defined('ABSPATH')) {
     exit; // Exit if accessed directly
 }
-
 function custom_curtain_options_enqueue_scripts() {
     if (is_product()) {
         global $product;
-
-        // Enqueue the CSS file
-        wp_enqueue_style('custom-curtain-options-css', plugin_dir_url(__FILE__) . '../assets/css/custom-curtain-options.css', array(), '1.0.0');
-
-        // Ensure $product is available and get the product type
+         wp_enqueue_style('custom-curtain-options-css', plugin_dir_url(__FILE__) . '../assets/css/custom-curtain-options.css', array(), '1.0.0');      
         if ($product && $product->is_type('simple')) {
             $product_id = $product->get_id();
             $product_type = get_post_meta($product_id, '_product_type', true);
-
-            // Conditionally enqueue the JavaScript file based on the product type
             if (!empty($product_type) && $product_type === 'rollover_tarps') {
                 wp_enqueue_script('roll-curtain-options', plugin_dir_url(__FILE__) . '../assets/js/roll-curtain-options.js', array('jquery'), '1.0.0', true);
             } elseif (!empty($product_type) && $product_type === 'livestock_curtains') {
