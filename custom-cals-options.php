a62f2a3a4e72892ccacfaa930f8e3ef1ffc3afcc
mufaqar <mufaqar@gmail.com>
2024-11-16 18:39:21 +0500

    Custom Calc

--- custom-cals-options.php
+++ custom-cals-options.php
@@ -14,13 +14,11 @@ if (!defined('ABSPATH')) {
     exit; // Exit if accessed directly
 }
 
-// Include necessary files
 //include_once plugin_dir_path(__FILE__) . 'includes/enqueue-scripts.php';
 include_once plugin_dir_path(__FILE__) . 'includes/custom-options.php';
 include_once plugin_dir_path(__FILE__) . 'includes/cart-checkout.php';
 include_once plugin_dir_path(__FILE__) . 'includes/admin-settings.php';
 include_once plugin_dir_path(__FILE__) . 'includes/expiry-date-calculator.php';
-
 
 function custom_product_options_enqueue_scripts() {
     wp_enqueue_script(
